e42aaa37a7cd81ade1f99017cdb3eb1cf7244eb3
Danial Hadavi <danialhadavi@gmail.com>
2020-04-17 11:56:29 -0400

    finished mvp working on styling

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { SmurfContext } from "../contexts/SmurfContext";
+import SmurfCard from "./SmurfCard";
+
+const SmurfList = () => {
+  const { smurfs, deleteSmurf } = useContext(SmurfContext);
+  return (
+    <div className="List">
+      {console.log(smurfs)}
+      {smurfs.map((smurf) => (
+        <SmurfCard key={smurf.id} smurf={smurf} deleteSmurf={deleteSmurf} />
+      ))}
+    </div>
+  );
+};
+
+export default SmurfList;
